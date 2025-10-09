d7ebab57b561c4a9141caa11cd9fbead31979c7b
Daniele Pizzarelli <danielepizz31@gmail.com>
2025-10-09 18:46:17 +0200

    Css fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader,'css-loader'],
             },
             {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -26,6 +27,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
+        new MiniCssExtractPlugin({ filename: 'styles.css' })
     ],
     mode: 'production',
     // devtool: 'source-map',
